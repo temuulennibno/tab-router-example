7c3efd50dfeb0878a270265e89c230e52e1439b5
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-01-15 17:18:52 +0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { Home } from "./screens/Home";
+import { About } from "./screens/About";
+import { HomeIcon } from "./icons/HomeIcon";
+import { InfoIcon } from "./icons/InfoIcon";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ tabBarActiveTintColor: "red", headerShown: false, tabBarItemStyle: { marginBottom: -30 } }}>
+        <Tab.Screen name="Home" component={Home} options={{ tabBarIcon: ({ color }) => <HomeIcon color={color} />, tabBarShowLabel: false }} />
+        <Tab.Screen name="About" component={About} options={{ tabBarIcon: ({ color }) => <InfoIcon color={color} />, tabBarShowLabel: false }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ icons/HomeIcon.tsx
@@ -0,0 +1,13 @@
+import { Path, Svg } from "react-native-svg";
+
+export const HomeIcon = ({ color }: { color?: string }) => {
+  if (!color) color = "#2c3e50";
+  return (
+    <Svg width="38" height="38" viewBox="0 0 24 24" stroke-width="1.5" stroke={color} fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <Path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <Path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+      <Path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+      <Path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+    </Svg>
+  );
+};
